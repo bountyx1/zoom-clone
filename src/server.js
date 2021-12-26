e6ef7e49abc51990fda1112e75f329cad8b1559c
bountyx1 <70125661+bountyx1@users.noreply.github.com>
2021-12-26 16:25:44 +0530

    basic setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
-console.log("Hello");
+// Set template rendering engine
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views" );
 
+// Static file serving
+app.use('/public', express.static(__dirname+ "/public"));
+
+app.get('/', (req, res) => res.render("home"));
 app.listen("3000")
