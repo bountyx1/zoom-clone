2be8bbaab473f404b9066ac8f7ac95f570f26052
bountyx1 <70125661+bountyx1@users.noreply.github.com>
2021-12-29 01:08:43 +0530

    Basic of websocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,13 @@
-alert("Haaaa");
+const socket = new WebSocket(`ws://${location.host}`);
+
+socket.addEventListener("open", ()=>{
+    console.log("Connected to server");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log(`Message recieved from server ${message.data}`);
+});
+
+socket.addEventListener("open", ()=>{
+    console.log("disconnected to server");
+});
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -10,4 +12,16 @@ app.set("views", __dirname + "/views" );
 app.use('/public', express.static(__dirname+ "/public"));
 
 app.get('/', (req, res) => res.render("home"));
-app.listen("3000")
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+    console.log("Connected to browser");
+    socket.send("Hi there how you doing ?");
+    socket.on("close", () => console.log("Disconnected to browser"));
+
+    socket.on("message", (message) => console.log(message.toString()));
+});
+
+server.listen("3000")
